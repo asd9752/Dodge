9f92f98e153a0c6f5521df7d02e1e3e2b6e99f24
asd9752 <vaaeew@gmail.com>
2022-09-14 17:37:20 +0900

    0914
    방향키 입력

--- Dodge/Assets/Scenes/PlayerController.cs
+++ Dodge/Assets/Scenes/PlayerController.cs
@@ -14,13 +14,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow) == true)
-        {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {   //위쪽방향키 입력이 감지된 경우 z방향 힘주기
             playerRigidbody.AddForce(0f, 0f, speed);
         }
         if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
+        {   //아래쪽방향키 입력이 감지된 경우 -z방향 힘주기
             playerRigidbody.AddForce(0f, 0f,-speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {   //오른쪽방향키 입력이 감지된 경우 x방향 힘주기
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {   //왼쪽방향키 입력이 감지된 경우 -x방향 힘주기
+            playerRigidbody.AddForce(-speed, 0f, 0f);
         }
     }
 }
