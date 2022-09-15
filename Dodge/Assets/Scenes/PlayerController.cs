9fe3ec8653702291487c98a0b13a8ede939d46d8
asd9752 <vaaeew@gmail.com>
2022-09-15 17:32:35 +0900

    0915
    die 메소드 추가 및 리지드바디 컴포넌트 자동 입력 구현

--- Dodge/Assets/Scenes/PlayerController.cs
+++ Dodge/Assets/Scenes/PlayerController.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
-    public float speed = 8f;
+   private Rigidbody playerRigidbody;
+   public float speed = 8f;
+
     void Start()
     {
-        
+        GetComponent<Rigidbody>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per framE
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow) == true)
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {   //아래쪽방향키 입력이 감지된 경우 -z방향 힘주기
-            playerRigidbody.AddForce(0f, 0f,-speed);
+            playerRigidbody.AddForce(0f, 0f, -speed);
         }
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {   //오른쪽방향키 입력이 감지된 경우 x방향 힘주기
@@ -30,5 +32,11 @@ public class PlayerController : MonoBehaviour
         {   //왼쪽방향키 입력이 감지된 경우 -x방향 힘주기
             playerRigidbody.AddForce(-speed, 0f, 0f);
         }
+        
+
+    }
+    public void die()
+    {
+        gameObject.SetActive(false);
     }
 }
