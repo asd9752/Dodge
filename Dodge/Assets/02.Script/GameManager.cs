559ee0bc2fb0c833f4be6f67fe1b56e02ee8e6de
asd9752 <vaaeew@gmail.com>
2022-09-23 12:36:15 +0900

    0923Am
    게임 메니저 메소드 완성
    endgame 메소드

--- Dodge/Assets/02.Script/GameManager.cs
+++ Dodge/Assets/02.Script/GameManager.cs
@@ -9,8 +9,8 @@ using TMPro;//TestMeshPro 관련 라이브러리
 public class GameManager : MonoBehaviour
 {
     public GameObject gameoverText;//게임오버시 활성화할 텍스트 게임 오브젝트
-    public Text timeText;// 생존 시간ㅇ을 표시할 텍스트 컴포넌트
-    public Text recordText;//최고 기록을 표시할 텍스트 컴포넌트
+    public TextMeshProUGUI timeText;// 생존 시간을 표시할 텍스트 컴포넌트
+    public TextMeshProUGUI recordText;//최고 기록을 표시할 텍스트 컴포넌트
 
     private float surviveTime;// 생존시간
     private bool isGameover; // 게임오버 상태
@@ -28,10 +28,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!isGameover)
+        {
+            //생존시간 확인
+            surviveTime += Time.deltaTime;
+            //갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
+            timeText.text = "Time: " + (int)surviveTime;
+        }
+        else
+        {
+            //게임오버 상태에서 R키를 누른 경우
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+              SceneManager.LoadScene("Main Scenes");
+            }
+        }
 
     }
     public void EndGame()
     {
-
+        //현재 상태를 게임 오버상태로 전환
+        isGameover = true;
+        //게임 오버 텍스트 게임오브젝트를 활성화
+        gameoverText.SetActive(true);
     }
 }
